0ab1a64be0fbea9439c59b8b5bc12b386235bcce
robertkrimen <robertkrimen@gmail.com>
2009-05-19 09:50:52 -0700

    Collection selection

--- assets/root/static/js.js
+++ assets/root/static/js.js
@@ -9,9 +9,15 @@ function applet( uri ) {
 
     refresh();
 
+    $( ".collection-list-item" ).livequery( 'click', function(event) {
+        var match = /collection-list-item-(\w+)/.exec( event.target.id );
+        if (match)
+            selectCollection( match[1] );
+    } );
+
 }
 
-function updateCollectionList( uri ) {
+function updateCollectionList( select ) {
     var name = Context.collection;
     $.getJSON( baseURI + "/collection/list", function( data ) {
         $( "#copy-into" ).html( Jemplate.process( "toolbar_select", { id: "copy-into-select", name: "Copy into", options: data.list } ) );
@@ -21,19 +27,25 @@ function updateCollectionList( uri ) {
         $.each( data.list, function() {
             $( "#collection-list" ).append( Jemplate.process( "collection_list_item", { name: this } ) );
         } );
-        $( ".collection-list-item" ).removeClass( "selected" );
-        $( "#collection-list-item-" + name ).addClass( "selected" );
+
+        selectCollection( select );
+    } );
+}
+
+function selectCollection( name ) {
+    $( ".collection-list-item" ).removeClass( "selected" );
+    $( "#collection-list-item-" + name ).addClass( "selected" );
+
+    $.getJSON( baseURI + "/collection/" + name + "/item/list", function( data ) {
+        $( "#item-list li" ).remove();
+        $.each( data.list, function() {
+            $( "#item-list" ).append( Jemplate.process( "item_list_item", { uri: baseURI, name: this } ) );
+        } );
     } );
 }
 
 function refresh() {
     var name = Context.collection;
 
-    updateCollectionList();
-
-    $.getJSON( baseURI + "/collection/" + name + "/item/list", function( data ) {
-        $.each( data.list, function() {
-            $( "#item-list" ).append( Jemplate.process( "item_list_item", { uri: baseURI, name: this } ) );
-        } );
-    } );
+    updateCollectionList( Context.collection );
 }
